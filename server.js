d3c523a3cd32b9b4ae23b7fa8b745cd1803809a5
Hallenbeck <RCHallen92@gmail.com>
2020-04-15 00:20:56 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+var app = express();
+var PORT= process.env.PORT || 5000;
+
+// Ejs
+// ================================================================================================
+app.use(expressLayouts);
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/files', express.static(__dirname + '/public/files'));
+
+
+
+// app.use(express.static(__dirname + '/public'));
+
+app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/whatInput', express.static('node_modules/what-input/dist'));
+app.use('/foundation', express.static('node_modules/foundation-sites/dist'));
+app.use('/', require(__dirname + '/routes/html-routes'));
+
+// app.use('/public', express.static('./public'));
+
+
+// require("./routes/api-routes.js")(app);
+
+
+
+
+app.listen(PORT, console.log("server start on " + PORT));
